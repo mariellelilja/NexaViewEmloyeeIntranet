5e1faf54078bc6383dee5c4890bd110a2b4cdc69
Marielle Lilja <56651394+mariellelilja@users.noreply.github.com>
2024-02-18 20:28:59 +0100

    Added helper to fix new references on run

---
+++ IntraUmbracoProject/IntraUmbracoProject/Helpers/AssetHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Linq;
+
+namespace IntraUmbracoProject.Helpers
+{
+    public class AssetHelper
+    {
+        private readonly IWebHostEnvironment _environment;
+
+        public AssetHelper (IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public string GetAssetPath(string folder, string pattern)
+        {
+            var assetDirectory = Path.Combine(_environment.WebRootPath, folder);
+            if (!Directory.Exists(assetDirectory))
+            {
+                return null;
+            }
+
+            var assetFile = Directory.GetFiles(assetDirectory, pattern).FirstOrDefault();
+            if(assetFile == null) {
+                return null;
+            }
+
+            return "/" + folder + "/" + Path.GetFileName(assetFile);
+        }
+    }
+}
