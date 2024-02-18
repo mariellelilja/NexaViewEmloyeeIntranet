40f8bbee524a6f2f929a5a3081ea58747f0cea69
Marielle Lilja <56651394+mariellelilja@users.noreply.github.com>
2024-02-18 21:59:21 +0100

    added loading indicator

---
+++ IntraUmbracoProject/IntraUmbracoProject/ClientApp/app/src/components/LoadingIndicator.vue
@@ -0,0 +1,70 @@
+<template>
+    <div v-if="isLoading" class="loading-container text-primary" role="status">
+      <div class="spinner">
+        <svg viewBox="0 0 50 50">
+           <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+        </svg>
+      </div>
+    </div>
+    <p>Loading...</p>
+</template>
+
+<script>
+export default {
+    name:'LoadingIndicator',
+    props: {
+    isLoading: Boolean
+  }
+}
+</script>
+  
+<style scoped>
+
+  .loading-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100px;
+  }
+  
+  .spinner {
+    width: 30px;
+    height: 30px;
+    stroke: #5652bf;
+    stroke-linecap: round;
+    animation: spin 3s linear infinite;
+  }
+  
+  .spinner .path {
+    stroke: #5652bf;
+    stroke-linecap: round;
+    animation: spin linear infinite;
+/* 
+    stroke-linecap: round;
+    stroke-dasharray: 80;
+    stroke-dashoffset: 0; */
+
+  }
+  .spinner svg {
+  display: block
+}
+  
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+      stroke-dasharray: 1, 200;
+      stroke-dashoffset: 0;
+    }
+    50% {
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -35px;
+    }
+    100% {
+      transform: rotate(360deg);
+      stroke-dasharray: 89, 200;
+      stroke-dashoffset: -124px;
+    }
+  }
+  </style>
+  
--- IntraUmbracoProject/IntraUmbracoProject/ClientApp/app/src/components/LoginForm.vue
+++ IntraUmbracoProject/IntraUmbracoProject/ClientApp/app/src/components/LoginForm.vue
@@ -1,44 +1,56 @@
 <template>
-    <div class="login-form-container">
-      <h2 class="form-title">Login to Employee Intranet</h2>
-      <form @submit.prevent="handleSubmit" class="login-form">
-        <div class="form-group">
-          <label for="username" class="form-label">Username:</label>
-          <input type="text" id="username" v-model="formData.username" required class="form-control">
-        </div>
-        <!-- <div class="form-group">
-          <label for="email" class="form-label">Email:</label>
-          <input type="email" id="email" v-model="formData.email" required class="form-control">
-        </div> -->
-        <div class="form-group">
-          <label for="password" class="form-label">Password:</label>
-          <input type="password" id="password" v-model="formData.password" required class="form-control">
-        </div>
-        <AppButton type="submit" :buttonId="'loginButton'" :buttonText="'Log in'" :styleClass="['m-3','nvi-btn', 'nvi-btn-primary']" ></AppButton>
-      </form>
-      <p class="mt-5, m-4">Not registered yet? </p>
-      <!---TODO style btn-wide and set @click to route to login:-->
-      <AppButton :buttonId="'toRegistrationBtn'" :buttonText="'Register'" :styleClass="['nvi-btn', 'nvi-btn-secondary', 'nvi-btn-wide']" ></AppButton>
+  <div class="login-form-container">
+    <h2 class="form-title">Login to Employee Intranet</h2>
+    <form @submit.prevent="handleSubmit" class="login-form" >
+      <div class="form-group">
+        <label for="username" class="form-label">Username: </label>
+        <input type="text" id="username" v-model="formData.username" :class="{'input-error': errors.username}" required class="form-control">
+        <div v-if="errors.username" class="error-message">{{ errors.username }}</div>
+      </div>
+      <!-- <div class="form-group">
+        <label for="email" class="form-label">Email:</label>
+        <input type="email" id="email" v-model="formData.email" required class="form-control">
+      </div> -->
+      <div class="form-group">
+        <label for="password" class="form-label">Password: </label>
+        <input type="password" id="password" v-model="formData.password" :class="{'input-error': errors.password}" required class="form-control">
+        <div v-if="errors.password" class="error-message">{{ errors.password }}</div>
+      </div>
+      <AppButton :disabled="isLoading" type="submit" :buttonId="'loginButton'" :buttonText="'Log in'" :styleClass="['m-3','nvi-btn', 'nvi-btn-primary']" ></AppButton>
+      <div class="feedback-container">
+        <LoadingIndicator :isLoading="isLoading" ></LoadingIndicator>
+      </div>
+    </form>
+    <p class="mt-5 m-4">Not registered yet? </p>
+    <!---TODO style btn-wide and set @click to route to login:-->
+    <AppButton  :buttonId="'toRegistrationBtn'" :buttonText="'Register'" :styleClass="['nvi-btn', 'nvi-btn-secondary', 'nvi-btn-wide']" ></AppButton>
+  </div>
+</template>
 
-    </div>
-  </template>
 
   
   <script>
   import AppButton from './AppButton.vue';
   import apiService from '@/services/apiService';
+  import LoadingIndicator from './LoadingIndicator.vue';
   
   export default {
     name: 'MainContent',
     props: {},
-    components: { AppButton},
+    components: { AppButton, LoadingIndicator},
     data() {
       return {
         formData: {
           username: '',
-          email: '',
+          // email: '',
           password: ''
-        }
+        },
+        errors: {
+          username: 'This user does not exist', //hardcoded for demo purpose, until fixed
+          password: ''
+        },
+        isLoading: true,
+        errorMessage: ''
       };
     },
     methods: { /* TODO move auth logic? */
@@ -120,5 +132,16 @@
   background-color: #3e8e41;
   box-shadow: none;
 } */
+
+/** TODO move */
+.input-error {
+  border-color: #ff3860; /* Red border color */
+}
+
+.error-message {
+  color: #ff3860; /* Red text color */
+  margin-top: 0.25rem;
+  font-size: 0.875rem;
+}
 
   </style>
