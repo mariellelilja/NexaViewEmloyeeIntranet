fadb80f73f462a18681f19fed8840a42c14053d3
Marielle Lilja <56651394+mariellelilja@users.noreply.github.com>
2024-02-21 13:35:25 +0100

    corrected automated static ref update

--- IntraUmbracoProject/IntraUmbracoProject/Helpers/AssetHelper.cs
+++ IntraUmbracoProject/IntraUmbracoProject/Helpers/AssetHelper.cs
@@ -13,20 +13,40 @@ namespace IntraUmbracoProject.Helpers
             _environment = environment;
         }
 
-        public string GetAssetPath(string folder, string pattern)
+        public IEnumerable<string> GetAllAssetPaths(string folder)
         {
             var assetDirectory = Path.Combine(_environment.WebRootPath, folder);
             if (!Directory.Exists(assetDirectory))
             {
-                return null;
+                yield break;
             }
 
-            var assetFile = Directory.GetFiles(assetDirectory, pattern).FirstOrDefault();
-            if(assetFile == null) {
-                return null;
+            var assetFiles = Directory.GetFiles(assetDirectory, "*.*", SearchOption.AllDirectories);
+            foreach (var assetFile in assetFiles)
+            {
+                var relativePath = assetFile.Replace(_environment.WebRootPath, "").Replace("\\", "/");
+                yield return relativePath;
             }
+        }
 
-            return "/" + folder + "/" + Path.GetFileName(assetFile);
+        public string GenerateHtmlTagForAsset(string assetPath)
+        {
+            var extension = Path.GetExtension(assetPath).ToLower();
+            switch (extension)
+            {
+                case ".css":
+                    return $"<link rel=\"stylesheet\" href=\"{assetPath}\">";
+                case ".js":
+                    return $"<script src=\"{assetPath}\"></script>";
+                case ".png":
+                case ".jpg":
+                case ".jpeg":
+                case ".gif":
+                case ".svg":
+                    return $"<img src=\"{assetPath}\" alt=\"\">";
+                default:
+                    return "";
+            }
         }
     }
 }
